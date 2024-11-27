41160439b28ec0e31ab7a09cf2b58baec7ae73a1
Gaman Sai <gamannallamothu@gmail.com>
2024-11-27 11:40:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cors(
 ));
 app.use(bodyParser.json());
 app.use(cookieParser());
-mongoose.connect("MONGO_URI="mongodb+srv://21bd1a661ecsma:GmU8n7fCcr9y9FiD@cluster0.tez0z.mongodb.net/");
+connectDB();
 connectRedis();
 
 const limiter = rateLimit({
